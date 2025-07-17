fc7d15077f06f53b08a9ebd3b9103cc2aef726cf
Halid Yıldırım <48795375+SMDHuman@users.noreply.github.com>
2025-07-17 18:19:54 +0300

    Update hh_darray.h

--- include/hh_darray.h
+++ include/hh_darray.h
@@ -2,7 +2,9 @@
 // Anoter dynamic array library for general purpose use
 // Use "#define HH_DARRAY_IMPLEMENTATION" ones in your .c file to
 // implement the functions of the library
-// github.com/SMDHuman
+//-----------------------------------------------------------------------------
+// Author		: github.com/SMDHuman
+// Last Update	: 17.07.2025
 //-----------------------------------------------------------------------------
 #ifndef HH_DARRAY_INIT_SIZE
 #define HH_DARRAY_INIT_SIZE 16
@@ -19,13 +21,13 @@
 typedef struct hh_darray_t{
 	size_t size; // How much of it available in bytes
 	size_t fill; // How much of it used in bytes
-	char word; // Word size in bytes
+	size_t word; // Word size in bytes
 	struct hh_darray_t *next; // Next darray for expention
 	void *data; // Data buffer location 	
 }hh_darray_t;
 
 // Initialize the array
-void hh_darray_init(hh_darray_t* array, char word); 
+void hh_darray_init(hh_darray_t* array, size_t word); 
 // Deinitialize the array
 void hh_darray_deinit(hh_darray_t* array); 
 // Add the item to end of the array
@@ -44,13 +46,15 @@ void hh_darray_pop(hh_darray_t* array, size_t index, void* item);
 size_t hh_darray_get_fill(hh_darray_t* array); 
 // Get total space allocated for it in bytes
 size_t hh_darray_get_size(hh_darray_t* array); 
+// Get how much of it is filled in item count
+size_t hh_darray_get_item_fill(hh_darray_t* array);
 #endif
 
 //-----------------------------------------------------------------------------
 // hh_darray function implementations
 #ifdef HH_DARRAY_IMPLEMENTATION
 
-void hh_darray_init(hh_darray_t* array, char word){
+void hh_darray_init(hh_darray_t* array, size_t word){
 	array->size = word*HH_DARRAY_INIT_SIZE;
 	array->fill = 0;
 	array->word = word;
@@ -100,7 +104,7 @@ void hh_darray_popend(hh_darray_t* array, void* item){
 }
 //-----------------------------------------------------------------------------
 void hh_darray_get(hh_darray_t* array, size_t index, void* item){
-	if(index > array->size / array->word){
+	if(index >= array->size / array->word){
 		if(array->next){
 			index -= array->fill / array->word;
 			hh_darray_get(array->next, index, item);	
@@ -111,7 +115,7 @@ void hh_darray_get(hh_darray_t* array, size_t index, void* item){
 }
 //-----------------------------------------------------------------------------
 void hh_darray_set(hh_darray_t* array, size_t index, void* item){
-	if(index > array->size / array->word){
+	if(index >= array->size / array->word){
 		if(array->next){
 			index -= array->fill / array->word;
 			hh_darray_set(array->next, index, item);	
@@ -135,5 +139,9 @@ size_t hh_darray_get_size(hh_darray_t* array){
 	}else{
 		return(array->size);
 	}
+}
+//-----------------------------------------------------------------------------
+size_t hh_darray_get_item_fill(hh_darray_t* array){
+	return(hh_darray_get_fill(array) / array->word);
 }
 #endif
