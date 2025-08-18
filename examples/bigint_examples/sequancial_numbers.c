0c24ebd9da624ad2e982f9c02b69e8f19d41fd23
SMDHuman <scrapmechanic1231@gmail.com>
2025-08-18 19:04:25 +0300

    New functions for bigint

--- examples/bigint_examples/sequancial_numbers.c
+++ examples/bigint_examples/sequancial_numbers.c
@@ -6,11 +6,12 @@ int main(){
     printf("Hello Numbers\n");
     hh_bigint_t a; hh_bigint_init(&a, 5);
     hh_bigint_t b; hh_bigint_init(&b, 3);
-    hh_bigint_t c; hh_bigint_init(&c, 0);
+    hh_bigint_t c; hh_bigint_init(&c, 123);
 
     //uint32_t val = 0xffffffff;
     //hh_bigint_set_buffer(&b, &val, 4);
 
+    hh_bigint_print_hex(&c);
     hh_bigint_add(&a, &b, &c);
     hh_bigint_print_hex(&c);
     hh_bigint_subtract(&a, &b, &c);
@@ -19,6 +20,9 @@ int main(){
     hh_bigint_multiply(&a, &b, &c);
     hh_bigint_print_hex(&c);
 
+
+    hh_bigint_convert_from_string(&a, "0b100110101001001001");
+    hh_bigint_print_hex(&a);
 
     hh_bigint_deinit(&a);
     hh_bigint_deinit(&b);
